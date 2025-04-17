81254d07cbec5bcc905582ad9bb03d45dc860aae
qallegue <qiuyi.allegue@gmail.com>
2025-04-17 17:28:56 -0400

    37

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, EventEmitter, input,Input, Output } from '@angular/core';
 
 
 @Component({
@@ -9,16 +9,24 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!:string;
-  @Input() name!:string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string; 
+  @Input({required: true}) id!: string;
+  @Output() select = new EventEmitter<string>();
 
-  get imagePath()
-  {
-    return 'assets/users/' + this.avatar;
-  }
+  // imagePath = computed( () =>
+  // {
+  //   return 'assets/users/' + this.avatar;
+  // });
+
+  get imagePath() {return './assets/users/' + this.avatar};
+
+
   onSelectUser()
   {
-    
+    this.select.emit(this.id);
   }
 
 }

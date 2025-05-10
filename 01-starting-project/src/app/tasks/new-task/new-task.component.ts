b8c19e616bed0d2db1678a175909bfcc75a27106
qallegue <qiuyi.allegue@gmail.com>
2025-05-10 17:22:24 -0400

    #54

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,27 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 @Output() canceled = new EventEmitter<void>();
-
+newTaskTitle = '';
+newTaskSummary = '';
+newTaskDueDate = '';
 onCancelButtonClick()
 {
   this.canceled.emit();
 }
+
+onSubmit()
+{
+  
 }
+
+}
+
